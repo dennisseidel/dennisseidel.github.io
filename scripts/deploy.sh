d8af4343086a589e7c5d225a5cdbf00cdff0b0f2
Dennis Seidel <den.seidel@gmail.com>
2019-08-16 22:23:49 +0200

    fix pipeline

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git remote set-url --push origin https://denseidel:${GITHUB_TOKEN}@github.com/denseidel/denseidel.github.io.git 
+git remote set-url origin https://denseidel:${GITHUB_TOKEN}@github.com/denseidel/denseidel.github.io.git 
 cd website
 npm install 
 GIT_USER=denseidel CURRENT_BRANCH=source yarn run publish-gh-pages
